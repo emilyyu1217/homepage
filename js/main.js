77479e4f9da55243aa2056930a2dac974f6e8e9a
Helge Bruegner <helge@bruegner.de>
2016-09-20 22:54:05 -0400

    Commented main js, removed old stuff, added default

--- js/main.js
+++ js/main.js
@@ -1,42 +1,39 @@
-// todo: history, onhashchange
-
-// Loading inspired by https://css-tricks.com/rethinking-dynamic-page-replacing-content/
+// some configurations
 var contentBasePath = "content/";
 var pagesPath = contentBasePath + "pages.html";
 var articleBasePath = contentBasePath + "articles/";
 var imageBasePath = contentBasePath + "images/";
 
-var baseHeight = 0;
 var fadeOutSpeed = 100, fadeInSpeed = fadeOutSpeed;
-
+var defaultSubpage = "#about";
 var activeClass = "active";
 
+// adds events, loads content initially
 $(function() {
-    var main = $("main");
-    var wrap = $("wrap");
+    // add event for hash changes
+    $(window).hashchange(reloadContent());
 
-    wrap.height(wrap.height());
-    baseHeight = wrap.height() - main.height();
-
-    $(window).hashchange(hashchange);
-
-    hashchange();
+    // load content initially
+    reloadContent();
 });
 
-hashchange = function() {
+// reloads the content of the UI
+// inspired by https://css-tricks.com/rethinking-dynamic-page-replacing-content/
+reloadContent = function() {
+    // get some UI elements
     var main = $("main"), cont = main.find("#content"), wrap = $("wrap"), nav = $("nav"), nav_a = $("nav a");
-    var hash = location.hash;
 
+    // get hash from URL or fallback
+    var hash = location.hash || defaultSubpage;
+
+    // fade out, load new content, fade in 
     cont.fadeOut(fadeOutSpeed, function() {
         cont.load(pagesPath + " " + hash, function() {
-            cont.fadeIn(fadeInSpeed, function() {
-                cont.animate({
-                    height: cont.height() + "px"
-                });
-            });
+            cont.fadeIn(fadeInSpeed);   
         });
     });
 
+    // update active link element
     nav_a.removeClass(activeClass);
     $("nav a[href$='" + hash + "']").addClass(activeClass);
 };
