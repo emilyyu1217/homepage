e8e8d40380fa875bc6e72b84ef3a31bd53d87e91
Helge Bruegner <helge@bruegner.de>
2016-09-20 21:56:36 -0400

    Added basic layout including single page loading

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// todo: history, onhashchange
+
+// Loading inspired by https://css-tricks.com/rethinking-dynamic-page-replacing-content/
+var contentBasePath = "content/";
+var pagesPath = contentBasePath + "pages.html";
+var articleBasePath = contentBasePath + "articles/";
+var imageBasePath = contentBasePath + "images/";
+
+var baseHeight = 0;
+var fadeOutSpeed = 200, fadeInSpeed = fadeOutSpeed;
+
+var activeClass = "active";
+
+$(function() {
+    var main = $("main");
+    var wrap = $("wrap");
+
+    wrap.height(wrap.height());
+    baseHeight = wrap.height() - main.height();
+
+    $(window).hashchange(hashchange);
+
+    hashchange();
+});
+
+hashchange = function() {
+    var main = $("main"), cont = main.find("#content"), wrap = $("wrap"), nav = $("nav"), nav_a = $("nav a");
+    var hash = location.hash;
+
+    cont.fadeOut(fadeOutSpeed, function() {
+        cont.load(pagesPath + " " + hash, function() {
+            cont.fadeIn(fadeInSpeed, function() {
+                cont.animate({
+                    height: cont.height() + "px"
+                });
+            });
+        });
+    });
+
+    nav_a.removeClass(activeClass);
+    $("nav a[href$='" + hash + "']").addClass(activeClass);
+};
